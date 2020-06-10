ac658cb6f8a621d721e2adb75976ef0dde51c41d
Syrka van Kuppenveld <syrkavankuppenveld@gmail.com>
2020-06-10 15:52:14 +0200

    removed grid file from __init__ file in classed/

--- code/classes/__init__.py
+++ code/classes/__init__.py
@@ -1,6 +1,5 @@
 
 from code.classes.gates import Gate
 from code.classes.chip import Chip
-from code.classes.grid import Grid
 from code.classes.wire import Wire
 from code.classes.graph import Graph
