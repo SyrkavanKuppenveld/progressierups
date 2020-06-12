7e0e1248af2092e7c0fe6332c54f2f2fc91b8730
Syrka van Kuppenveld <syrkavankuppenveld@gmail.com>
2020-06-12 11:23:29 +0200

    plane greedy test

--- code/algorithms/__init__.py
+++ code/algorithms/__init__.py
@@ -1,2 +1,3 @@
 from code.algorithms.random_eline import RandomEline
-from code.algorithms.greedy_eline import Greedy
+from code.algorithms.greedy_eline import Greedy_eline
+from code.algorithms.greedy import Greedy
--- code/algorithms/greedy_eline.py
+++ code/algorithms/greedy_eline.py
@@ -1,24 +1,24 @@
 
 
-class Greedy():
+class Greedy_eline():
 
     def __init__(self, gates, connections):
         """Initializes a Greedy object."""
-
-        gates = self.gates
-        connections = self.connections
-
-    def get_nextGate(self):
-
-        # Compute gate density
-        for gate in self.gates:
-            self.gates[gate].get_density(False)
-
-        
-        
-        
-
         pass
+    #     gates = self.gates
+    #     connections = self.connections
 
-    def run(self):
-        pass
+    # def get_nextGate(self):
+
+    #     # Compute gate density
+    #     for gate in self.gates:
+    #         self.gates[gate].get_density(False)
+
+        
+        
+        
+
+    #     pass
+
+    # def run(self):
+    #     pass
