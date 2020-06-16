349d0681b8f881bbd008dda8d96a79a33e7cca07
Syrka van Kuppenveld <syrkavankuppenveld@gmail.com>
2020-06-16 16:46:00 +0200

    added comments and docstrings to greedy.py

--- code/algorithms/greedy.py
+++ code/algorithms/greedy.py
@@ -1,67 +1,123 @@
-# import numpy as numpy
-
-from code.classes.node import Node
 from code.visualization.visualize import Chip_Visualization
 
 class Greedy():
-    """ Class that creates wires according to the shortest Manhattan Distance.
+    """ Provide Object to perform the Greedy algorithm with.
     
+    Net order
+    ---------
     The algorithm starts with building wires between the gate with number "1" as
-    ID and its connections. It will continue building connections according to the 
-    ID-numbers of the gates. Paths to the gates' corresponding connection will also
-    be made based upon the ID of the connected gate. It will first walk along the 
-    x-dimension and subsequently along the y-dimension of the chip.
+    ID and its connected gates. Paths to the connected gates will also be made in 
+    ascending order of the ID of the connected gate. It will continue building 
+    connections according to the ID-numbers of the gates.  
+    
+    Path
+    ----
+    The algorithm will first walk along the Manhattan distance of the x-dimension and 
+    subsequently along Manhattan distance of the y-dimension of the chip.
+    
+    w.r.t. = with respect to
     ********************************************************************************
     """
     
     def __init__(self, graph):
+        """ Retrieves a graph environment for the algorithm to navigate in.
+
+        Parameters
+        ----------
+        graph : Graph object of the used chip and one of its netlists
+        """
+        
         self.graph = graph
         
-        # Store already connected gate-net-duo
+        # Storage of already connected gate-net-duo's
         self.completed_connections = set()
         
-        # Store wire paths (key = tuple of gate duo, value = list of coordinates)
+        # Dictionary of wire paths (key = tuple of gate duo, value = list of coordinates)
         self.total_path = {}
 
-    
-    def determine_gate_order(self):
-        ordered_gates = []
+    def determine_net_order(self):
+        """ Returns order in which the nets will be build (list)."""
         
+        gates = []
+        
+        # Create tuple of gateID and Gate Object to enable sorting
         for gate in self.graph.connections:
-            ordered_gates.append((gate.gateID, gate))
+            gates.append((gate.gateID, gate))
 
-        ordered_gates = sorted(ordered_gates, key=lambda x:x[0])
+        # Sort gates by ID
+        ordered_gates = sorted(gates, key=lambda x:x[0])
         
         return ordered_gates
-
     
     def compute_steps(self, current_gate_coords, connected_gate_coords):
+        """ Computes the lowest number of steps needed to get to the connected gate.
+        
+        Parameters
+        ----------
+        current_gate_coords : Coordinates (x, y, z) of the current gate
+        connected_gate_coords : Coordinates (x, y, z) of the to be connected gate
+
+        Return
+        ------
+        steps_x : The lowest number of steps needed to get to the connected gate
+        w.r.t. the x-axes
+        steps_y : The lowest number of steps needed to get to the connected gate
+        w.r.t. the y-axes
+        
+        """
+        
         steps_x = abs(current_gate_coords[0] - connected_gate_coords[0])
         steps_y = abs(current_gate_coords[1] - connected_gate_coords[1])
-
-        return steps_x, steps_y
+        
+        return (steps_x, steps_y)
 
     def compute_rel_Manhattandist(self, current_gate_coords, connected_gate_coords):
+        """ Computes the relative distance between the current gate and the to be 
+        connected gate.
+        
+        Parameters
+        ----------
+        current_gate_coords : Coordinates (x, y, z) of the current gate
+        connected_gate_coords : Coordinates (x, y, z) of the to be connected gate
+
+        Return
+        ------
+        relative_dist_x : Relative number of lengt-units between the current gate 
+        and the to be connected gate w.r.t. the x-axes
+        relative_dist_y : Relative number of lengt-units between the current gate 
+        and the to be connected gate w.r.t. the x-axes
+        
+        """
+        
         relative_dist_x = connected_gate_coords[0] - current_gate_coords[0]
         relative_dist_y = connected_gate_coords[1] - current_gate_coords[1]
 
-        return relative_dist_x, relative_dist_y
+        return (relative_dist_x, relative_dist_y)
     
     def create_path(self, start_coordinates, relative_dist_x, relative_dist_y, connected_gate_coords):
-
-        path = []
-        x_coord, y_coord = start_coordinates[0], start_coordinates[1]
+        """ Builds shortest possible path according to the connected gate.
         
-        # Since steps are made according to the Manhatten distance, the z_coordinate
-        # will always be zero.
-        z_coord = 0
+        Parameters
+        ----------
+        start_coordinates : Coordinates (x, y, z) of the start of the path (= current gate)
+        relative_dist_x : Relative Manhattan distance between the gates w.r.t. the x-axis 
+        reltive_dist_y : Relative Manhattan distance between the gates w.r.t. the y-axis
+        connected_gate_coords : Coordinates (x, y, z) of the endpoint of the path (= connected gate)
+        
+        Return
+        ------
+        path : Coordinates of the created path (list)
+        """
+        
+        path = []
+        path.append(start_coordinates)
+        x_coord, y_coord, z_coord = start_coordinates[0], start_coordinates[1], start_coordinates[2]
 
         current_coords = start_coordinates
         goal_coords = connected_gate_coords
 
-        # Walk along the x-dimension
+        # Walk along the x-axis
         if relative_dist_x != 0:
-            print(f"relative dist x: {relative_dist_x}")
             while current_coords[0] != goal_coords[0]:
                 if relative_dist_x > 0:
                     x_coord += 1
@@ -70,9 +126,8 @@ class Greedy():
                 current_coords = (x_coord, y_coord, z_coord)
                 path.append(current_coords)
 
-        # Walk along the y-dimension
+        # Walk along the y-axis
         if relative_dist_y != 0:
-            print(f"relative dist y: {relative_dist_y}")
             while current_coords[1] != goal_coords[1]:
                 if relative_dist_y > 0:
                     y_coord += 1
@@ -84,44 +139,39 @@ class Greedy():
         return path
     
     def run(self):
-        """Returns wire path."""
+        """Runs the Greedy algorithm to build the wire path with.
+        
+        Return
+        ------
+        total_path = Dictionary (key = net) of paths between the gates (value: list of coordinates)
+        """
 
-        # Get a list of gates ordered by ID
-        gate_order = self.determine_gate_order()
+        # Get a list of gates sorted by ID
+        gate_order = self.determine_net_order()
 
         # Build wire paths
-        for index, gate in enumerate(gate_order):
+        for gate in gate_order:
 
             current_gateID, current_gateObject = gate
             connections = list(self.graph.connections[current_gateObject])
             
-            # Create path for each connection of the current gate
+            # Build paths to each to be connected gate of the current gate
             for index, connection in enumerate(connections):
                 connected_gateObject = connection
                 connected_gateID = connected_gateObject.gateID
     
                 current_path = []
 
-                # Determine gate duo (= tuple)
-                gate_duo = tuple(sorted([current_gateID, connected_gateID]))
-                # print(f"Gate duo: {gate_duo}")
+                # Get net (= tuple)
+                net = tuple(sorted([current_gateID, connected_gateID]))
 
-                # Do not continue if a path between the current and connected gate has already been made
-                if gate_duo not in self.completed_connections:
+                # Do not continue if the net has already been completed
+                if net not in self.completed_connections:
 
                     # Get coordinates (= tuple: (x, y, z))
                     current_gate_coords = (current_gateObject.xcoord, current_gateObject.ycoord, current_gateObject.zcoord)
                     connected_gate_coords = (connected_gateObject.xcoord, connected_gateObject.ycoord, connected_gateObject.zcoord)
-
-                    # print(f"Current gate coord: {current_gate_coords}")
-                    # print(f"Connected gate coord: {connected_gate_coords}")
-
-                    # Add start coordinates to the current path
-                    current_path.append(current_gate_coords)
-
-                    # Compute steps
-                    # steps_x, steps_y = self.compute_steps(current_gate_coords, connected_gate_coords)
-
+                    
                     # Compute relative Manhattan distances
                     relative_dist_x, relative_dist_y = self.compute_rel_Manhattandist(current_gate_coords, connected_gate_coords)
                     
@@ -130,17 +180,15 @@ class Greedy():
                     current_path.extend(current_path_coordinates)
 
                     # Append current path to the total path
-                    self.total_path[gate_duo] = current_path
+                    self.total_path[net] = current_path
 
                     # Update the completed connections set
-                    self.completed_connections.add(gate_duo)
+                    self.completed_connections.add(net)
 
                     # Visulalize chip on each path of the algorithm
-                    print(f"gate duo: {gate_duo}")
-                    visualisation = Chip_Visualization(self.graph.gates, self.total_path)
-                    visualisation.run()
+                    # visualisation = Chip_Visualization(self.graph.gates, self.total_path)
+                    # visualisation.run()
         
-        print(f"total path: {self.total_path}")
         return self.total_path
 
                 
