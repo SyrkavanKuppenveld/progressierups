5bbf2524a416f87168ed29fe5b609f8f69db04c6
elinevangroningen <elinevangroningen@gmail.com>
2020-06-16 21:12:32 +0200

    working random and restruct main

--- code/algorithms/random.py
+++ code/algorithms/random.py
@@ -1,80 +1,166 @@
 import random
-import copy
-from code.classes.node import Node
+from code.classes.wire import Wire
 from code.visualization.visualize import Chip_Visualization
-# from class_eline import Chip_Visualization
 
 class Random():
+    """ 
+    Creates a Wire object that connects the gates according to the netlist and 
+    according to the lowest Manhattan Distance. 
+
+    Random element
+    --------------
+    * The order of the connections are generated randomly.
+    * If multiple neighbors have the same distance, the next position is generated
+      randomly. 
+    
+    Greedy element
+    --------------
+    The next position will be the neighbor with the lowest Manhattan distance.
+    """
 
     def __init__(self, graph):
+        """
+        Initializes the Random Greedy Net algorithm.
+        
+        Parameters
+        ----------
+        graph: a Graph object
+                A Graph object representing the chip grid.
+        """
         
         self.graph = graph
-        self.wire = set()        
+        self.wire = Wire()   
 
-    def check_collision(self, position, step):
-        """Returns True if no collision, otherwise False."""
-
-        position_coords = position.xcoord, position.ycoord, position.zcoord
-        step_coords =  step.xcoord, step.ycoord, step.zcoord
+    def get_next_connection(self, connections):
+        """Randomly returns a connection.
         
-        step = tuple(sorted((position_coords, step_coords)))
+        Parameters
+        ----------
+        connections: a list
+                A list containing the connections from the netlist file.
 
-        return step not in self.wire
-    
+        Returns
+        -------
+        tuple
+                Randomly generated tuple containing two gates that need to be connected.
+        """
+
+        return connections.pop(random.randrange(0, len(connections)))
+
+    def get_next_neighbor(self, neighbors):
+        """Randomly returns a neighbor.
+        
+        Parameters
+        ----------
+        connections: a list
+                A list containing the neighbors of the current position of the wire.
+
+        Returns
+        -------
+        Node object
+                Randomly generated neighbor Node.
+        """
+
+        return neighbors.pop(random.randrange(0, len(neighbors)))
+
+    def next_position(self, position, goal):
+        """
+        Returns the next position, according to the lowest Manhattan Distance.
+                
+        Parameters
+        ----------
+        position: a Node object
+                A Node object representing the current position of the wire.
+
+        goal: a Node object
+                A Node object representing the goal position.
+        
+        Returns
+        -------
+        Node object
+                The Node object that will be the new position of the wire.
+        """
+
+        neighbors = list(position.neighbors)
+
+        while neighbors:
+            neighbor = self.get_next_neighbor(neighbors)
+            if self.wire.check_collision(position, neighbor):
+                return neighbor
+
+        raise ValueError
+
     def make_connection(self, gate_a, gate_b):
-        """Returns wire connection between gate_a and gate_b."""
+        """
+        Returns set with wire path between gate_a and gate_b.
         
-        connection = []
+        Parameters
+        ----------
+        gate_a: int
+                A integer representing the gateID of gate_a.
 
-        # Get position and goal nodes
+        gate_a: int
+                A integer representing the gateID of gate_b.
+
+
+        Returns
+        -------
+        tuple
+                A tuple containing the wire path to connect gate_a and gate_b.
+        """
+        
+        wire_path = []
+
+        # Get correspoding nodes for position(gate_a) and goal(gate_b)
         position = self.graph.nodes[(gate_a.xcoord, gate_a.ycoord, gate_a.zcoord)]
         goal = self.graph.nodes[(gate_b.xcoord, gate_b.ycoord, gate_b.zcoord)]
 
-        # Add position
-        connection.append((position.xcoord, position.ycoord, position.zcoord))
+        # Add start position to wire path
+        wire_path.append((position.xcoord, position.ycoord, position.zcoord))
 
         # Iterate until connection has been made
         while position != goal:
-            allowed_neighbors = []
-
-            # Iterate over neighbors current position
-            for neighbor in position.neighbors:
-
-                # If move is allowed compute and append Manhattan Distance
-                if self.check_collision(position, neighbor) and (neighbor.isgate is False or neighbor == goal):
-                    allowed_neighbors.append(neighbor)
             
-            # Assign new position
+            # Store current position in temporary value
             tmp = position
-            position = random.choice(allowed_neighbors)
 
-            # Add wire to path
-            tmp_coords = tmp.xcoord, tmp.ycoord, tmp.zcoord
-            position_coords = position.xcoord, position.ycoord, position.zcoord
-            wire_path = tuple(sorted((tmp_coords, position_coords)))
-            self.wire.add(wire_path)
+            # Generate next position
+            position = self.next_position(position, goal)
 
-            # Append connection
-            connection.append((position.xcoord, position.ycoord, position.zcoord))
+            # Update wire path and coordinates
+            self.wire.update_path(tmp, position)
+            self.wire.update_coords(position)
 
-        return tuple(connection)
-
+            # Append step to wire path
+            wire_path.append((position.xcoord, position.ycoord, position.zcoord))
+        
+        return tuple(wire_path)
+   
     def run(self):
+        """
+        Returns dict with the wire route to connect all gates according to netlist.
+
+        Returns
+        -------
+        dict 
+                A dictionary containing the route of the wire per connection.
+        """
 
         route = {}
-        completed = set()
         netlist = list(self.graph.netlist)
 
+        # Iterate until netlist is empyt
         while netlist:
+
+            # Get random connection 
             connection = netlist.pop(random.randrange(0, len(netlist)))
+
+            # Get corresponding Gate objects
             a, b = connection[0], connection[1]
             gate_a, gate_b = self.graph.gates[a], self.graph.gates[b]
-            route[(a, b)] = self.make_connection(gate_a, gate_b)
-            completed.add((a, b))
-            visualisation = Chip_Visualization(self.graph.gates, route)
-            visualisation.run()
-    
-        return route
-      
 
-        
+            # Generate the connection between gate a and b
+            route[(a, b)] = self.make_connection(gate_a, gate_b)
+
+        return route
+
--- main.py
+++ main.py
@@ -1,19 +1,136 @@
-from code.classes import Gate
-from code.classes.graph_new import Graph
-from code.algorithms.eline_algo import Algorithm
+from code.classes import Graph
+from code.algorithms import Greedy_RandomNet, Greedy_RandomNet_LookAhead, Random
 from code.visualization import Chip_Visualization
 
 if __name__ == "__main__":
 
-    print_file = "example/print_0.csv"
-    netlist_file = ["example/netlist_1.csv"]
+    print_file = "gates&netlists/chip_0/print_0.csv"
+    netlist_file = "gates&netlists/chip_0/netlist_1.csv"
+    netlist_file = "gates&netlists/chip_0/netlist_2.csv"
+    # netlist_file = "gates&netlists/chip_0/netlist_3.csv"
 
+    # print_file = "gates&netlists/chip_1/print_1.csv"
+    # netlist_file = "gates&netlists/chip_1/netlist_4.csv"
+    # netlist_file = "gates&netlists/chip_1/netlist_5.csv"
+    # netlist_file = "gates&netlists/chip_1/netlist_6.csv"
+
+    # print_file = "gates&netlists/chip_2/print_2.csv"
+    # netlist_file = "gates&netlists/chip_2/netlist_7.csv"
+    # netlist_file = "gates&netlists/chip_2/netlist_8.csv"
+    # netlist_file = "gates&netlists/chip_2/netlist_9.csv"
+
+    # Instantiate Graph object
     graph = Graph(print_file, netlist_file)
 
-    # Perform Algorithm
-    algo = Algorithm(graph)
-    wire_path = algo.run()
-    print(wire_path)
 
-    visualisation = Chip_Visualization(graph.gates, wire_path)
-    visualisation.run()
+    # -------------------------- RANDOM --------------------------
+
+    # # Perform algorithm once, print costs and visualise
+    # algo = Random(graph)
+    # wire_path = algo.run()
+    # costs = algo.wire.compute_costs()
+    # print(f'wire costs = {costs}')
+    # visualisation = Chip_Visualization(graph.gates, wire_path)
+    # visualisation.run()
+
+    # # Repeat algorithm until solution is found
+    # not_found = True
+    # while not_found:
+    #     while True:
+            
+    #         # Restart algorithm if error occurs
+    #         try:
+    #             # Run algorithm
+    #             algo = Random(graph)
+    #             wire_path = algo.run()
+
+    #             # Compute and print wire costs
+    #             costs = algo.wire.compute_costs()
+    #             print(f'wire costs = {costs}')
+
+    #             # Visualise algorithm 
+    #             visualisation = Chip_Visualization(graph.gates, wire_path)
+    #             visualisation.run()
+
+    #             # Set not_found to True and break out of loop
+    #             not_found = False
+    #             break
+    #         except:
+    #             print("restart algorithm")
+    #             break    
+
+
+    # -------------------- RANDOM GREEDY NET --------------------
+    
+    # # Perform algorithm once, print costs and visualise
+    # algo = Greedy_RandomNet(graph)
+    # wire_path = algo.run()
+    # costs = algo.wire.compute_costs()
+    # print(f'wire costs = {costs}')
+    # visualisation = Chip_Visualization(graph.gates, wire_path)
+    # visualisation.run()
+
+    # # Repeat algorithm until solution is found
+    # not_found = True
+    # while not_found:
+    #     while True:
+    # 
+    #         # Restart algorithm if error occurs
+    #         try:
+    #             # Run algorithm
+    #             algo = Greedy_RandomNet(graph)
+    #             wire_path = algo.run()
+
+    #             # Compute and print wire costs
+    #             costs = algo.wire.compute_costs()
+    #             print(f'wire costs = {costs}')
+
+    #             # Visualise algorithm 
+    #             visualisation = Chip_Visualization(graph.gates, wire_path)
+    #             visualisation.run()
+
+    #             # Set not_found to True and break out of loop
+    #             not_found = False
+    #             break
+    #         except:
+    #             print("restart algorithm")
+    #             break
+
+
+# --------------- RANDOM GREEDY NET LOOK AHEAD ---------------
+
+    # # Perform algorithm once print costs and visualise
+    # algo = Greedy_RandomNet_LookAhead(graph)
+    # wire_path = algo.run()
+    # costs = algo.wire.compute_costs()
+    # print(f'wire costs = {costs}')
+    # visualisation = Chip_Visualization(graph.gates, wire_path)
+    # visualisation.run()
+
+    # # Repeat algorithm until solution is found
+    # not_found = True
+    # while not_found:
+    #     while True:
+    #
+    #         # Restart algorithm if error occurs
+    #         try:
+    #             # Run algorithm
+    #             algo = Greedy_RandomNet_LookAhead(graph)
+    #             wire_path = algo.run()
+
+    #             # Compute and print wire costs
+    #             costs = algo.wire.compute_costs()
+    #             print(f'wire costs = {costs}')
+
+    #             # Visualise algorithm 
+    #             visualisation = Chip_Visualization(graph.gates, wire_path)
+    #             visualisation.run()
+
+    #             # Set found to True and break out of loop
+    #             not_found = False
+    #             break
+    #         except:
+    #             print("restart algorithm")
+    #             break
+
+
