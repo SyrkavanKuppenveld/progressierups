4c689773175b8ad88a3efaf2e055cef385a9d531
Syrka van Kuppenveld <syrkavankuppenveld@gmail.com>
2020-06-18 13:37:41 +0200

    intermediate (not yet working) version of hillclimber

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -8,11 +8,12 @@ class HillClimber(Greedy_RandomNet_LookAhead):
     <INFORMATIE>
     """
 
-    def __init__(self, random_startState, start_wire_path, start_cost):
+    def __init__(self, graph, random_startState, start_wire_path, start_cost):
         """ 
         Initializes the start state of the algorithm.
         """
     
+        self.graph = graph
         self.wire = random_startState
         self.wire_path = start_wire_path
         self.cost = start_cost
@@ -30,15 +31,51 @@ class HillClimber(Greedy_RandomNet_LookAhead):
         # Randomly choose a net that is to be re-build
         nets = list(self.wire_path.keys())
         net = random.choice(nets)
-        return net
-    
-    def remove_net(self, net):
-        pass
-
-
-    def apply_random_adjustment(self, net):
-        gate_a, gate_b = net
         
+        gate_a = self.graph.gates[net[0]]
+        gate_b = self.graph.gates[net[1]]
+
+        gates = (gate_a, gate_b)
+
+        return net, gates
+    
+    def remove_net(self, net, gates):
+        
+        print(f"Net: {net}")
+        
+        net_path = self.wire_path[net]
+        print(f"Net path: {net_path}")
+        
+        coord_storage = []
+        for coordinates in net_path:    
+            # Remove old path coordinates from the coordinate storage of the Wire Object
+            # Gates can never be an intersection and thus will not be taken into account
+            node = self.graph.nodes[coordinates]
+            print(f"Node = {node}")
+            print(f"Type = {type(node)}")
+            if not node.isgate:
+                print(f"coordinates: {coordinates}")
+                print(f"wire coords: {self.wire.coords}")
+                print(f"test")
+                
+                # Remove node form coordinate storage
+                self.wire.coords.remove(node)
+
+                # Correct intersection-count of the Node object of the current coordinate
+                node.decrement_intersection()
+                
+                # Keep track of wire-units in old path
+                coord_storage.append(coordinates)
+                
+                # Create wire unit
+                if len(coord_storage) == 2:
+                    # Remove old wire units from the path storage of the Wire Object
+                    self.wire.path.remove((coord_storage[0], coord_storage[1]))
+                    coord_storage.pop(0)
+
+
+    def apply_random_adjustment(self, net, gates):
+        gate_a, gate_b = gates
         new_path = self.make_connection(gate_a, gate_b)
         
         # Update wire_path
@@ -60,13 +97,13 @@ class HillClimber(Greedy_RandomNet_LookAhead):
 
             # 3. Apply random adjustment
             # Get random net
-            net = self.get_random_net()
+            net, gates = self.get_random_net()
 
             # Remove old net form wire object
-            self.remove_net(net)
+            self.remove_net(net, gates)
 
             # Apply random adjustment on net
-            self.apply_random_adjustment(net)
+            self.apply_random_adjustment(net, gates)
             
             break
             # pass
